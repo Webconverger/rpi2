f8f0f7520593bf97ecc4dc1b9014c98c65b33e93
Kai Hendry <hendry@webconverger.com>
2015-04-02 16:28:00 +0800

    Basic Webconverger debug system

---
+++ usr/lib/cmake/PulseAudio/PulseAudioConfig.cmake
@@ -0,0 +1,10 @@
+set(PULSEAUDIO_FOUND TRUE)
+
+set(PULSEAUDIO_VERSION_MAJOR 6)
+set(PULSEAUDIO_VERSION_MINOR 0)
+set(PULSEAUDIO_VERSION 6.0)
+set(PULSEAUDIO_VERSION_STRING "6.0")
+
+find_path(PULSEAUDIO_INCLUDE_DIR pulse/pulseaudio.h HINTS "/usr/include")
+find_library(PULSEAUDIO_LIBRARY NAMES pulse libpulse HINTS "/usr/lib")
+find_library(PULSEAUDIO_MAINLOOP_LIBRARY NAMES pulse-mainloop-glib libpulse-mainloop-glib HINTS "/usr/lib")
